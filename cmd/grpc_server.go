68b000830eab55a71990df33b92516eec82f6a0e
programzheng <programzheng@gmail.com>
2022-07-25 00:29:27 +0800

    :speech_balloon: modify run grpc server command summary

--- cmd/grpc_server.go
+++ cmd/grpc_server.go
@@ -12,13 +12,8 @@ import (
 // grpcServerCmd represents the grpcServer command
 var grpcServerCmd = &cobra.Command{
 	Use:   "grpcServer",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "run a grpc server",
+	Long:  `run a grpc server`,
 	Run: func(cmd *cobra.Command, args []string) {
 		grpc_server.Run()
 	},
